74c182b287107a4c891246894fbe365442d06f77
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-08 19:15:23 +1000

    Added Messages Resolver Mutations and Subscription

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,10 +1,18 @@
 const usersResolvers = require("./users");
+const messagesResolvers = require("./messages");
 
 module.exports = {
+   User: {
+      followerCount: (parent) => parent.followers.length,
+   },
    Query: {
       ...usersResolvers.Query,
    },
    Mutation: {
       ...usersResolvers.Mutation,
+      ...messagesResolvers.Mutation,
+   },
+   Subscription: {
+      ...messagesResolvers.Subscription,
    },
 };
