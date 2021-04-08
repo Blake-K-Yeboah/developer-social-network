658cd0da85b85b44b5bb1548b2b2c7724a6f5c0f
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-08 15:55:52 +1000

    Setup User Model

---
+++ models/User.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+   username: String,
+   password: String,
+   email: String,
+   createdAt: String,
+   followers: [
+      {
+         username: String,
+         followedAt: String,
+      },
+   ],
+});
+
+module.exports = model("User", userSchema);
