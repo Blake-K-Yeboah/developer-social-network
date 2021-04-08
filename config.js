cba9ebe0af20ec5cebea73c5b5f8b2cda8ff3ef9
Blake Yeboah <blakekyeboah@gmail.com>
2021-04-08 15:55:31 +1000

    Added Config File with database url and jwt secret

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+   MONGODB:
+      "mongodb+srv://admin:adminpassword@cluster0.ddstr.mongodb.net/development?retryWrites=true&w=majority",
+   SECRET_KEY: "I am  secret key",
+};
